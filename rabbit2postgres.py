981d62b298111113ef577711d5a9db729a730c8f
Leszek Marcjan <leszek.marcjan@algolytics.pl>
2017-03-17 16:09:00 +0100

    refaktoring skryptu

--- rabbit2postgres.py
+++ rabbit2postgres.py
@@ -4,91 +4,116 @@ import sys
 import pika
 import json
 from sets import Set
+import time
 
-if (len(sys.argv)!=10):
-  raise Exception("Illegal arguments count: "+str(len(sys.argv)))
 
-#rabbit_user='user'
-rabbit_user=sys.argv[1]
-#rabbit_password='bitnami'
-rabbit_password=sys.argv[2]
-#rabbit_host='localhost'
-rabbit_host=sys.argv[3]
-#rabbit_port=5672
-rabbit_port=int(sys.argv[4])
-#rabbit_queue='sce.postgres.data-queue'
-rabbit_queue=sys.argv[5]
+class PostgresHelper:
+    def __init__(self, postgres_dbname, postgres_user, postgres_password, postgres_host):
+        self.postgres_dbname = postgres_dbname
+        self.postgres_user = postgres_user
+        self.postgres_password = postgres_password
+        self.postgres_host = postgres_host
+        self.tableinfo = {}
+        self.data_buffer = []
+        self.max_data_buffer_size = 1000
 
-#postgres_dbname='postgres2'
-postgres_dbname=sys.argv[6]
-#postgres_user='postgres'
-postgres_user=sys.argv[7]
-#postgres_password='postgres'
-postgres_password=sys.argv[8]
-#postgres_host='localhost'
-postgres_host=sys.argv[9]
+    def create_postgres_connection(self):
+        self.conn = psycopg2.connect(
+            "dbname=" + self.postgres_dbname + " user=" + self.postgres_user + " password=" + self.postgres_password + " host=" + self.postgres_host)
+        self.cursor = self.conn.cursor()
+        print "Postgres connection created."
 
-max_data_buffer_size=1000
+    def insert_data(self, table, data):
+        self.tableinfo[table] = Set()
+        for k, v in data.items():
+            self.tableinfo[table].add(str(k))
+        self.data_buffer.append((table, data))
+        if len(self.data_buffer) >= self.max_data_buffer_size:
+            self.insert_data_buffer()
+            self.tableinfo.clear()
+            del self.data_buffer[:]
 
-conn = psycopg2.connect("dbname="+postgres_dbname+" user="+postgres_user+" password="+postgres_password+" host="+postgres_host)
-cursor = conn.cursor()
+    def insert_data_buffer(self):
+        for table_name, columns_insert in self.tableinfo.items():
+            try:
+                print "Inserting buffered data into table " + str(table_name) + "..."
+                q_create_table = "CREATE TABLE IF NOT EXISTS " + str(
+                    table_name) + " (id SERIAL PRIMARY KEY, " + " text default null, ".join(
+                    [str(k1) for k1 in columns_insert]) + " text default null)"
+                self.cursor.execute(q_create_table)
+                q_get_columns = "SELECT column_name FROM information_schema.columns WHERE table_name = '" + str(
+                    table_name) + "'"
+                self.cursor.execute(q_get_columns)
+                columns_existing = [k[0] for k in self.cursor.fetchall()]
+                for k in columns_insert:
+                    if not k.lower() in columns_existing:
+                        q_add_column = "ALTER TABLE " + str(table_name) + " ADD COLUMN " + str(k) + " text default null"
+                        self.cursor.execute(q_add_column)
+                c = 0
+                for d in self.data_buffer:
+                    if d[0] == table_name:
+                        q_insert = "INSERT INTO " + d[0] + " (" + ", ".join(
+                            [str(k) for k, v in d[1].items()]) + ") VALUES (" + ", ".join(
+                            [str(v) for k, v in d[1].items()]) + ");"
+                        self.cursor.execute(q_insert)
+                        c = c + 1
+                self.conn.commit()
+                print "Inserted " + str(c) + " rows."
+            except:
+                print "Unexpected error when inserting into table " + str(table_name), sys.exc_info()[0]
 
-tableinfo = {}
-data_buffer = []
-def insert_data(table, data):
-    tableinfo[table] = set()
-    for k,v in data.items():
-      tableinfo[table].add(str(k))
-    data_buffer.append((table, data))
-    if len(data_buffer) >= max_data_buffer_size:
-      insert_data_buffer()
-      tableinfo.clear()
-      del data_buffer[:]
 
-def insert_data_buffer():
-    for table_name,columns_insert in tableinfo.items():
-      try:
-        print "Inserting buffered data into table " + str(table_name) + "..."
-        q_create_table = "CREATE TABLE IF NOT EXISTS " + str(table_name) + " (id SERIAL PRIMARY KEY, " + " text default null, ".join([str(k1) for k1 in columns_insert]) + " text default null)"
-        cursor.execute(q_create_table)
-        q_get_columns = "SELECT column_name FROM information_schema.columns WHERE table_name = '" + str(table_name) + "'"
-        cursor.execute(q_get_columns)
-        columns_existing = [k[0] for k in cursor.fetchall()]
-        for k in columns_insert:
-          if not k.lower() in columns_existing:
-            q_add_column = "ALTER TABLE " + str(table_name) + " ADD COLUMN " + str(k) + " text default null"
-            cursor.execute(q_add_column)
-        c=0
-        for d in data_buffer:
-            if d[0] == table_name:
-              q_insert =  "INSERT INTO " + d[0] + " (" + ", ".join([str(k) for k,v in d[1].items() ]) + ") VALUES (" + ", ".join([str(v) for k,v in d[1].items() ]) + ");"
-              cursor.execute(q_insert)
-              c = c+1
-        conn.commit()
-        print "Inserted " + str(c) + " rows."
-      except:
-        print "Unexpected error when inserting into table " + str(table_name), sys.exc_info()[0]
+class RabbitHelper:
+    def __init__(self, rabbit_user, rabbit_password, rabbit_host, rabbit_port, rabbit_queue, postgres_helper):
+        self.rabbit_user = rabbit_user
+        self.rabbit_password = rabbit_password
+        self.rabbit_host = rabbit_host
+        self.rabbit_port = rabbit_port
+        self.rabbit_queue = rabbit_queue
+        self.postgres_helper = postgres_helper
+        self.credentials = pika.PlainCredentials(rabbit_user, rabbit_password)
+        self.parameters = pika.ConnectionParameters(rabbit_host, rabbit_port, '/', self.credentials)
 
-credentials = pika.PlainCredentials(rabbit_user, rabbit_password)
-parameters = pika.ConnectionParameters(rabbit_host,
-                                   rabbit_port,
-                                   '/',
-                                   credentials)
-connection = pika.BlockingConnection(parameters)
-channel = connection.channel()
+    def callback(self, ch, method, properties, body):
+        body_obj = json.loads(body)
+        table = body_obj["table"]
+        data = body_obj["data"]
+        self.postgres_helper.insert_data(table, data)
+        ch.basic_ack(delivery_tag=method.delivery_tag)
 
-channel.queue_declare(queue=rabbit_queue, durable=True)
-print(' [*] Waiting for messages. To exit press CTRL+C')
+    def create_pika_connection(self):
+        self.connection = pika.BlockingConnection(self.parameters)
+        self.channel = self.connection.channel()
+        self.channel.queue_declare(queue=self.rabbit_queue, durable=True)
+        self.channel.basic_qos(prefetch_count=1)
+        self.channel.basic_consume(self.callback, queue=self.rabbit_queue)
+        print "Pika connection created."
 
-def callback(ch, method, properties, body):
-    body_obj = json.loads(body)
-    table = body_obj["table"]
-    data = body_obj["data"]
-    insert_data(table, data)
-    ch.basic_ack(delivery_tag = method.delivery_tag)
+    def start_pika_consumer(self):
+        print(' [*] Waiting for messages. To exit press CTRL+C')
+        self.channel.start_consuming()
 
-channel.basic_qos(prefetch_count=1)
-channel.basic_consume(callback,
-                      queue=rabbit_queue)
 
-channel.start_consuming()
+if (len(sys.argv) != 10):
+    raise Exception("Invalid number of arguments: " + str(len(sys.argv)))
+
+postgres_helper = PostgresHelper(sys.argv[6], sys.argv[7], sys.argv[8], sys.argv[9])
+rabbit_helper = RabbitHelper(sys.argv[1], sys.argv[2], sys.argv[3], int(sys.argv[4]), sys.argv[5], postgres_helper)
+
+while True:
+    try:
+        postgres_helper.create_postgres_connection()
+        break
+    except:
+        print "Unexpected error when creating postgres connection", sys.exc_info()[0]
+        time.sleep(5)
+
+while True:
+    try:
+        rabbit_helper.create_pika_connection()
+        break
+    except:
+        print "Unexpected error when creating pika connection", sys.exc_info()[0]
+        time.sleep(5)
+
+rabbit_helper.start_pika_consumer()
